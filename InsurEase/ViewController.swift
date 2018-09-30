b5049dbe9df40b6ca41a83bc2bf0de43ac7f5f15
Kenton Li <kentonli622@icloud.com>
2018-10-01 00:18:18 +0800

    Initial Commit

---
+++ InsurEase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InsurEase
+//
+//  Created by Kenton on 1/10/2018.
+//  Copyright © 2018 Prologue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
